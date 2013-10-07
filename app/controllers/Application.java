694aac9f9db85e89306d5b87537e3b68c25d11d9
diodfr <didier.broca@gmail.com>
2013-10-07 22:30:42 +0200

    Fix concurrent modification error

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,7 +9,7 @@ import views.html.index;
 import fr.diodfr.codestory.s3.e1.Elevator;
 
 public class Application extends Controller {
-	
+
 	private static final String ELEVATOR_KEY = "Elevator";
 
 	private static Elevator getElevatorInstance() {
@@ -25,43 +25,46 @@ public class Application extends Controller {
 			return new Elevator();
 		}
 	}
-	
+
 	private static void resetElevatorInstance() {
 		Cache.remove(ELEVATOR_KEY);
 	}
-	
-    public static Result index() {
-        return ok(index.render("Code Story s3 e1 : elevator on play framework"));
-    }
 
-    public static Result call(int atFloor, String to) {
-    	getElevatorInstance().addCall(atFloor, to);
-    	return ok();
-    }
-    
-    public static Result go(int floorToGo) {
-        getElevatorInstance().goTo(floorToGo);
-    	return ok();
-    }
+	public static Result index() {
+		return ok(index.render("Code Story s3 e1 : elevator on play framework"));
+	}
 
-    public static Result userHasExited() {
-    	getElevatorInstance().userExited();
-        return ok();
-    }
+	public static Result call(int atFloor, String to) {
+		getElevatorInstance().addCall(atFloor, to);
+		return ok();
+	}
 
-    public static Result userHasEntered() {
-        getElevatorInstance().userEntered();
-    	return ok();
-    }
-    
-    public static Result reset(String cause) {
-    	resetElevatorInstance();
-        return ok();
-    }
-    
+	public static Result go(int floorToGo) {
+		getElevatorInstance().goTo(floorToGo);
+		return ok();
+	}
+
+	public static Result userHasExited() {
+		getElevatorInstance().userExited();
+		return ok();
+	}
+
+	public static Result userHasEntered() {
+		getElevatorInstance().userEntered();
+		return ok();
+	}
+
+	public static Result reset(String cause) {
+		Elevator elevatorInstance = getElevatorInstance();
+		System.out.println("Application.reset()" + cause);
+		System.out.println(elevatorInstance.toString());
+		resetElevatorInstance();
+		return ok();
+	}
+
 	public static Result nextCommand() {
-        Elevator elevatorInstance = getElevatorInstance();
+		Elevator elevatorInstance = getElevatorInstance();
 		// System.out.println(elevatorInstance.toString());
-        return ok(elevatorInstance.nextCommand());
-    }
+		return ok(elevatorInstance.nextCommand());
+	}
 }
--- app/fr/diodfr/codestory/s3/e1/Elevator.java
+++ app/fr/diodfr/codestory/s3/e1/Elevator.java
@@ -2,6 +2,7 @@ package fr.diodfr.codestory.s3.e1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -22,120 +23,136 @@ public class Elevator {
 			this.floor = floor;
 			this.up = CMD_UP.equals(up);
 		}
-		
+
 		@Override
 		public String toString() {
 			return "f=" + floor + ";dir=" + (up?CMD_UP:CMD_DOWN);
 		}
 	}
-	
-	
+
+
 	public static class User {
 		private int destFloor;
-		
+
 		public User(int floor) {
 			this.destFloor = floor;
 		}
-		
+
 		@Override
 		public String toString() {
 			return "dest=" + destFloor;
 		}
 	}
-	
+
 	private boolean doorsOpened = false;
 	private int currentFloor = 0;
-	
+
 	private List<Call> calls = new ArrayList<Call>();
 	private List<User> users = new ArrayList<User>();
 
 	public void goTo(int floor) {
 		User user = new User(floor);
-		users.add(user );
+		synchronized (users) {
+			users.add(user);
+		}
 	}
-	
+
 	public void addCall(int floor, String destination) {
 		Call call = new Call(floor, destination);
-		calls.add(call);
+		synchronized (calls) {
+			calls.add(call);
+		}
 	}
-	
+
 	private boolean isDoorOpened() {
 		return doorsOpened;
 	}
-	
+
 	protected String openDoor() {
 		doorsOpened = true;
-		
+
 		return CMD_OPEN;
 	}
-	
+
 	private String closeDoor() {
 		doorsOpened = false;
-		
+
 		return CMD_CLOSE;
 	}
-	
+
 	private String up() {
 		currentFloor ++;
-		
+
 		return CMD_UP;
 	}
-	
+
 	private String down() {
 		currentFloor--;
-		
+
 		return CMD_DOWN;
 	}
-	
+
 	public void userExited() {
-		for (User user : users) {
-			if (user.destFloor == currentFloor) {
-				users.remove(user);
-				return;
+		synchronized (users) {
+			for (Iterator<User> usersIterator = users.iterator(); usersIterator.hasNext();) {
+				User user = usersIterator.next();
+
+				if (user.destFloor == currentFloor) {
+					usersIterator.remove();
+					return;
+				}
 			}
 		}
 	}
-	
+
 	public void userEntered() {
-		for (Call call : calls) {
-			if(call.floor == currentFloor) {
-				calls.remove(call);
-				return;
+		synchronized (calls) {
+			for (Iterator<Call> callsIterator = calls.iterator(); callsIterator.hasNext();) {
+				Call call = callsIterator.next();
+				if(call.floor == currentFloor) {
+					callsIterator.remove();
+					return;
+				}
 			}
 		}
 	}
-	
+
 	public String nextCommand() {
-		if (isDoorOpened()) {
-			return nextCommandCloseDoor();
-		} else if (!users.isEmpty()) {
-			return nextCommandUser();
-		} else if (!calls.isEmpty()) {
-			return nextCommandCalls();
+		synchronized (users) {
+			synchronized (calls) {
+
+				if (isDoorOpened()) {
+					return nextCommandCloseDoor();
+				} else if (!users.isEmpty()) {
+					return nextCommandUser();
+				} else if (!calls.isEmpty()) {
+					return nextCommandCalls();
+				}
+				
+				return CMD_NOTHING;
+			}
 		}
-		
-		return CMD_NOTHING;
 	}
-	
+
 	private String nextCommandCalls() {
 		int[] minMax = new int[2];
 		minCallFloor(minMax);
-		
+
 		int dest = Math.abs(currentFloor-minMax[0]) < Math.abs(currentFloor-minMax[1]) ? minMax[0]: minMax[1] ;
-		
+
 		if (dest == currentFloor) {
 			return openDoor();
 		} else if (dest > currentFloor) {
 			return up();
 		}
-		
+
 		return down();
 	}
 
 	private void minCallFloor(int[] minMax) {
 		minMax[0] = Integer.MAX_VALUE;
 		minMax[1] = Integer.MIN_VALUE;
-		
+
 		for (Call call : calls) {
 			if (call.floor < minMax[0] && call.up) {
 				minMax[0] = call.floor;
@@ -144,7 +161,7 @@ public class Elevator {
 				minMax[1] = call.floor;
 			}
 		}
-		
+
 		if (minMax[0] == Integer.MAX_VALUE) {
 			minMax[0] = minMax[1];
 		} else if (minMax[1] == Integer.MIN_VALUE) {
@@ -153,80 +170,80 @@ public class Elevator {
 	}
 
 	private String nextCommandUser() {
-//		int dest = currentFloor;
-//		int diffDest = Integer.MAX_VALUE;
-		
-//		for (User user : users) {
-//			int diffCurrent = Math.abs(currentFloor - user.destFloor);
-//		
-//			if (diffCurrent < diffDest) {
-//				diffDest = diffCurrent;
-//				dest = user.destFloor;
-//			}
-//		}
-		
+		//		int dest = currentFloor;
+		//		int diffDest = Integer.MAX_VALUE;
+
+		//		for (User user : users) {
+		//			int diffCurrent = Math.abs(currentFloor - user.destFloor);
+		//		
+		//			if (diffCurrent < diffDest) {
+		//				diffDest = diffCurrent;
+		//				dest = user.destFloor;
+		//			}
+		//		}
+
 		int dest = optimiseGoDest(createDestCount(), users.size());
-		
+
 		if (currentFloor == dest) {
 			return openDoor();
 		} else if (currentFloor < dest) {
 			if (findCall(currentFloor, true)) {
 				return openDoor();
 			}
-			
+
 			return up();
 		}
-		
+
 		if (findCall(currentFloor, false)) {
 			return openDoor();
 		}
-		
+
 		return down();
 	}
 
 	Map<Integer, Integer> createDestCount() {
 		Map<Integer, Integer> countMap = new TreeMap<Integer, Integer>();
-		
+
 		for (User user : users) {
 			Integer integer = countMap.get(user.destFloor);
 			countMap.put(user.destFloor, integer!= null? integer + 1 : 1);
 		}
-		
+
 		return countMap;
 	}
-	
+
 	int optimiseGoDest(Map<Integer,Integer> destCount, int userCount) {
 		int min = Integer.MAX_VALUE;
 		int dest = destCount.keySet().iterator().next();
-		
+
 		for (Entry<Integer,Integer> entry : destCount.entrySet()) {
 			TreeMap<Integer, Integer> tempMap = new TreeMap<Integer, Integer>(destCount);
 			tempMap.remove(entry.getKey());
 			int current = optimiseGoDest(currentFloor, entry.getKey(), entry.getValue(), tempMap, userCount);
-			
+
 			if (current < min) {
 				min = current;
 				dest = entry.getKey();
 			}
 		}
-		
+
 		return dest;
 	}
-	
+
 	private int optimiseGoDest(int currentPos, int floor, int count, Map<Integer, Integer> destCount, int userCount) {
 		int min = Integer.MAX_VALUE;
-		
+
 		for (Entry<Integer, Integer> entry : destCount.entrySet()) {
 			TreeMap<Integer, Integer> tempMap = new TreeMap<Integer, Integer>(destCount);
 			tempMap.remove(entry.getKey());
 			int remainingUserCount = userCount - count;
 			min = Math.min(min, optimiseGoDest(floor, entry.getKey(), entry.getValue(), tempMap, remainingUserCount) + remainingUserCount); // others drops + close door
 		}
-		
+
 		if (min == Integer.MAX_VALUE) {
 			min = 0;
 		}
-		
+
 		return min + ((Math.abs(floor - currentPos)+1) * userCount);
 	}
 
@@ -253,4 +270,3 @@ public class Elevator {
 		return status.toString();
 	}
 }
- 
